249440af42905b1cf5ae7c193f9eb09130d5b221
ANNAKondrashova <117078208+ANNAKondrashova@users.noreply.github.com>
2022-11-14 15:48:03 +0300

    Add files via upload

---
+++ ex13.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = num;
+int j = 0;
+ do{
+        j++;
+        num1 = num1/10;
+ }
+ while (num1 > 0);
+
+ if(j >- 3)
+ {
+    double step = Math.Pow(10, j-2);
+    double count = num % step;
+    double result = Math.Floor(count);
+    System.Console.WriteLine(Math.Floor(count/Math.Pow(10,j-3)));
+ }
+ else {
+    System.Console.WriteLine("Третьей циры нет");
+ }
+
---
+++ ex15.cs
@@ -0,0 +1,31 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+static void DayOffWeek(int dayWeek)
+{
+    switch (dayWeek)
+    {
+        case 1:
+            Console.WriteLine("Понедельник- НЕТ");
+            break;
+        case 2:
+            Console.WriteLine("Вторник- НЕТ");
+            break;
+        case 3:
+            Console.WriteLine("Среда- НЕТ");
+            break;
+        case 4:
+            Console.WriteLine("Четверг- НЕТ");
+            break;
+        case 5:
+            Console.WriteLine("Пятница- ДА");
+            break;
+        case 6:
+            Console.WriteLine("Суббота- НЕТ");
+            break;
+        case 7:
+            Console.WriteLine("Воскресенье- НЕТ");
+            break;
+        default:
+            Console.WriteLine("Некорректный номер дня недели");
+            break;
+    }
+}
